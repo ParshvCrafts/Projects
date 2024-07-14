3961d03b5340504d98d600f46c2bb0c190fe5823
ParshvCrafts <p1a2r3s4h5v6@gmail.com>
2024-07-14 13:04:36 -0700

    bomb_defuse.py

--- bomb_defuse.py
+++ bomb_defuse.py
@@ -1,17 +1,22 @@
 import random
 import time
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"  # To hide the Pygame introduction message
+import pygame
+from pygame import mixer   # py -m pip install pygame
+
 
 def main():
-    print("*** Defuse the Bomb ***")
-    print("You are a Bomb Defuser. Guess the correct password to defuse {0-9}. If you fail, you have 5 seconds to run away")
-    code= random.randint(0, 9)
-    enter=int(input(f"Enter correct code: "))
-    
     Clear_and_retrun= "\033c"
-
+    print(Clear_and_retrun)
+    print("***💥 Defuse the Bomb 💥***")
+    print("You are a Bomb Defuser. Guess the correct wire to defuse [black, red, blue]. If you fail, you have 5 seconds to run away")
+    code= random.choice(["black", "red", "blue"])
+    enter=input(f"Enter correct wire: ")
+    
     if enter==code:
        print(Clear_and_retrun)
-       print("Congratulations, bomb successfully defused! :>")
+       print("Congratulations, bomb successfully defused! ✅ ✅ ✅")
     else:
        n=5
        while n>-1:
@@ -20,5 +25,10 @@ def main():
          time.sleep(1)
          n=n-1
        print("💥💥💥, you lost")
+       mixer.init()
+       mixer.music.load(r"explosion.mp3") # Download the explosion.mp3 in the same directory to ply the sound
+       mixer.music.play()
+       while mixer.music.get_busy():  # wait for music to finish playing
+            time.sleep(1)
 
 main()
